f9c6f365f97623341a78d1db53336ae8d4a17fa3
Yan <yanxfu@gmail.com>
2024-06-05 10:51:29 +0800

    implement function to call Ali DNS Open API

--- main.sh
+++ main.sh
@@ -27,7 +27,7 @@ getMachineIpv6() {
     echon "fd17::1"
 }
 
-# Echo 给定的单个字符的 UTF-8 编码，比如参数为「夏」则返回「%e5%a4%8f」
+# Echo 给定的单个字符的 UTF-8 编码，比如参数为「夏」则返回「%E5%A4%8F」
 getUtf8Hex() {
     ch="$1"
     length=${#ch}
@@ -36,10 +36,11 @@ getUtf8Hex() {
         return
     fi
 
-    echon "$ch" | hexdump -ve '/1 "_%02x"' | tr '_' '%'
+    echon "$ch" | hexdump -ve '/1 "_%02X"' | tr '_' '%'
 }
 
-# Echo 给定的字符串的 url encode 之后的结果，支持包括中文在内的国际字符
+# Echo 给定的字符串的 url encode 之后的结果，支持包括中文在内的国际字符，
+# 比如参数为「www.例子.中国」，则返回「www.%E4%BE%8B%E5%AD%90.%E4%B8%AD%E5%9B%BD」
 urlEncode() {
     str="$1"
     chs=$(echon "$str" | sed -e 's/\(.\)/\1\n/g')
@@ -64,21 +65,51 @@ urlEncode() {
 
 # Param1: Canonical query string
 # Param2: 请求头中 "x-acs_action" 的值，也就是 API 名字，如 DescribeSubDomainRecords
-# Param3: 请求头中 "x-acs-date" 的值，如 2024-06-04T08:52:00Z
-# Param4: 请求头中 "x-acs-signature-nonce" 的值，如 00112233445566778899aabbccddeeff
+# Param3: Access Key ID
+# Param4: Access Key Secret
 # 根据这些参数 echo 一个 raw JSON
 callAliDnsOpenApi() {
+    canonicalQueryString="$1"
+    xAcsAction="$2"
+    accessKeyId="$3"
+    accessKeySecret="$4"
+
+    host="alidns.cn-hangzhou.aliyuncs.com"
+    xAcsContentSha256="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    xAcsDate=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+    xAcsSignatureNonce=$(awk 'BEGIN{srand(); for(i=0;i<4;i++) printf "%08x",int(rand()*"0xFFFFFFFF")}')
+    xAcsVersion="2015-01-09"
+    signedHeaders="host;x-acs-action;x-acs-content-sha256;x-acs-date;x-acs-signature-nonce;x-acs-version"
+
     canonicalRequest="POST
 /
-${1}
-host:alidns.cn-hangzhou.aliyuncs.com
-x-acs-action:${2}
-x-acs-date:${3}
-x-acs-signature-nonce: ${4}
-x-acs-version:2015-01-09
+${canonicalQueryString}
+host:${host}
+x-acs-action:${xAcsAction}
+x-acs-content-sha256:${xAcsContentSha256}
+x-acs-date:${xAcsDate}
+x-acs-signature-nonce:${xAcsSignatureNonce}
+x-acs-version:${xAcsVersion}
 
-host;x-acs-action;x-acs-date;x-acs-signature-nonce;x-acs-version
-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+${signedHeaders}
+${xAcsContentSha256}"
+    hashedCanonicalRequest=$(echon "$canonicalRequest" | openssl dgst -sha256 | tail -c 65 | head -c 64)
+    stringToSign="ACS3-HMAC-SHA256
+${hashedCanonicalRequest}"
+    signature=$(echon "$stringToSign" | openssl dgst -sha256 -hmac "$accessKeySecret" | tail -c 65 | head -c 64)
+
+    authorization="ACS3-HMAC-SHA256 Credential=${accessKeyId},SignedHeaders=${signedHeaders},Signature=${signature}"
+    url="${host}/?${canonicalQueryString}"
+    rawJson=$(curl -s -X POST \
+        -H "Authorization: $authorization" \
+        -H "host: $host" \
+        -H "x-acs-action: $xAcsAction" \
+        -H "x-acs-content-sha256: $xAcsContentSha256" \
+        -H "x-acs-date: $xAcsDate" \
+        -H "x-acs-signature-nonce: $xAcsSignatureNonce" \
+        -H "x-acs-version: $xAcsVersion" \
+        "$url")
+    echon "$rawJson"
 }
 
 # 调阿里的 Open API 获取单个 sub domain 的解析记录，echo 逗号分隔字符串，格式如下：
@@ -86,10 +117,13 @@ e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 # AAAA,fd17::1
 describeSubDomainRecords() {
     subDomain=$(urlEncode "$1")
-    # TODO: Complete this function
+    rawJson=$(callAliDnsOpenApi "SubDomain=${subDomain}" "DescribeSubDomainRecords" "$ACCESS_KEY_ID" "$ACCESS_KEY_SECRET")
+    # TODO: grep useful info from raw JSON
+    echon "$rawJson"
 }
 
-main() {
+# 读取配置文件，若配置文件不存在，则新建一个并退出
+readConfigFile() {
     configPath="$(getScriptDir)/config.sh"
     echo2 "准备读取配置文件：${configPath}"
     if [ ! -r "$configPath" ]; then
@@ -123,6 +157,10 @@ EOL
 
     # shellcheck disable=SC1090
     . "$configPath"
+}
+
+main() {
+    readConfigFile
 }
 
 main
