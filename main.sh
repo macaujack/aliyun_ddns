7190301c0c5c4534dd2143054ce644d71ec4cd42
Yan <yanxfu@gmail.com>
2024-06-05 23:01:01 +0800

    implement main function and add format to log

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-log() {
+log() (
     level="$1"
     text="$2"
     case "$level" in
@@ -18,33 +18,33 @@ log() {
     level=$(printf '%s' "$level" | tr '[:lower:]' '[:upper:]')
     date=$(date +%Y-%m-%dT%H:%M:%S)
     printf '%b\n' "${color}${date} <${level}> ${text}\033[0m" 1>&2
-}
+)
 
-echon() {
+echon() (
     printf '%s' "$1"
-}
+)
 
 # Echo 这个脚本所处的目录，最后不包含这个脚本的文件名
-getScriptDir() {
+getScriptDir() (
     script=$(readlink -f "$0")
     scriptDir=$(dirname "$script")
     echon "$scriptDir"
-}
+)
 
 # Echo 本机的 IPv4 地址
-getMachineIpv4() {
+getMachineIpv4() (
     # TODO: Complete this function
     echon "192.168.2.1"
-}
+)
 
 # Echo 本机的 IPv6 地址，会尽量选择永久（非临时）的地址
-getMachineIpv6() {
+getMachineIpv6() (
     # TODO: Complete this function
     echon "fd17::1"
-}
+)
 
 # Echo 给定的单个字符的 UTF-8 编码，比如参数为「夏」则返回「%E5%A4%8F」
-getUtf8Hex() {
+getUtf8Hex() (
     ch="$1"
     length=${#ch}
     if [ "$length" -ne 1 ]; then
@@ -55,11 +55,11 @@ getUtf8Hex() {
     enc=$(echon "$ch" | hexdump -ve '/1 "_%02X"' | tr '_' '%')
     log verb "UTF-8 of 「${ch}」 is 「${enc}」"
     echon "$enc"
-}
+)
 
 # Echo 给定的单行字符串的 url encode 之后的结果，支持包括中文在内的国际字符，
 # 比如参数为「www.例子.中国」，则返回「www.%E4%BE%8B%E5%AD%90.%E4%B8%AD%E5%9B%BD」
-urlEncode() {
+urlEncode() (
     str="$1"
     length=${#str}
     ret=""
@@ -84,16 +84,16 @@ urlEncode() {
         i=$((i + 1))
     done
 
-    log verb "URL encode for 「${str}」 is 「${ret}」"
+    log verb "URL encode for \\033[4m「${str}」\\033[24m is \\033[4m「${ret}」\\033[24m"
     echon "$ret"
-}
+)
 
 # Param1: Canonical query string
 # Param2: 请求头中 "x-acs_action" 的值，也就是 API 名字，如 DescribeSubDomainRecords
 # Param3: Access Key ID
 # Param4: Access Key Secret
 # 根据这些参数 echo 一个 raw JSON
-callAliDnsOpenApi() {
+callAliDnsOpenApi() (
     canonicalQueryString="$1"
     xAcsAction="$2"
     accessKeyId="$3"
@@ -102,7 +102,7 @@ callAliDnsOpenApi() {
     host="alidns.cn-hangzhou.aliyuncs.com"
     xAcsContentSha256="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     xAcsDate=$(date -u +%Y-%m-%dT%H:%M:%SZ)
-    xAcsSignatureNonce=$(awk 'BEGIN{srand(); for(i=0;i<4;i++) printf "%08x",int(rand()*"0xFFFFFFFF")}')
+    xAcsSignatureNonce=$(openssl rand -hex 32)
     xAcsVersion="2015-01-09"
     signedHeaders="host;x-acs-action;x-acs-content-sha256;x-acs-date;x-acs-signature-nonce;x-acs-version"
 
@@ -143,26 +143,26 @@ ${hashedCanonicalRequest}"
     fi
     log verb "🥰 HTTP 请求成功"
     echon "$rawJson"
-}
+)
 
 # 用来检查返回的 raw JSON 里是否不存在 "Code" 字段，若不存在则为成功
 # Param1: Raw JSON
-checkIfApiCallSuccess() {
+checkIfApiCallSuccess() (
     echon "$rawJson" | grep -E '"Code" *: *"[a-zA-Z0-9.]+"'
     retVal=$?
     if [ "$retVal" -eq 0 ]; then
         log error "HTTP 请求成功，但是存在业务错误（比如参数不合法）。原始 JSON 返回值为「${rawJson}」"
         return 1
     fi
-}
+)
 
 # 获取一个 JSON 对象（不可是数组）的一个 key 对应的值（该值类型必须为字符串）
 # Param1: Raw JSON
 # Param2: key 的名字
-getJsonStringValueOfKey() {
+getJsonStringValueOfKey() (
     regex=$(printf 's/^.*"%s" *: *"([^"]+)".*$/\\1/p' "$2")
     echon "$1" | sed -nE "$regex"
-}
+)
 
 # 调阿里的 Open API 获取单个 sub domain 的解析记录，echo 一个用制表符分隔的多行字符串，一行表示一种类型的记录，
 # 需要注意第 2 个字段是 RR（即主机记录），由于这个函数是用来查询某个给定 SubDomain 的所有记录，因此结果里每行的
@@ -171,15 +171,21 @@ getJsonStringValueOfKey() {
 # 666666660000000000\twww\tA\t192.168.2.1
 # 666666660000000001\twww\tAAAA\tfd17::1
 # 666666660000000002\twww\tTXT\tsometext
-describeSubDomainRecords() {
+describeSubDomainRecords() (
     subDomain=$(urlEncode "$1") || return 1
     rawJson=$(callAliDnsOpenApi "SubDomain=${subDomain}" "DescribeSubDomainRecords" "$ACCESS_KEY_ID" "$ACCESS_KEY_SECRET") || return 2
     checkIfApiCallSuccess "$rawJson" || return 3
 
-    records=$(echon "$rawJson" | sed -nE 's/^.*"Record" *: *(\[.+\]).*$/\1/p')
+    records=$(echon "$rawJson" | sed -nE 's/^.*"Record" *: *(\[.*\]).*$/\1/p')
     if [ ${#records} -eq 0 ]; then
         log error "HTTP 请求成功且没有业务错误，但是无法匹配预期的字符串，请联系脚本作者检查服务器返回的 JSON 格式和匹配规则"
+        log error "原始 JSON 返回值为「${rawJson}」"
         return 4
+    fi
+
+    if [ "$records" = '[]' ]; then
+        log warn "当前不存在域名「${1}」的任何类型的解析记录"
+        return
     fi
 
     records=$(echon "$records" | grep -oE '\{[^}]+\}')
@@ -197,22 +203,22 @@ ${records}
 EOL
 
     echon "$ret"
-}
+)
 
 # 调阿里的 Open API 更新单个解析记录。
 # Param1: Record ID
 # Param2: RR，（即主机记录，比如 www, @, 测试），支持包括中文在内的国际字符
 # Param3: Type （即记录类型，比如 A, AAAA, CNAME, TXT）
 # Param4: Value
-updateDomainRecord() {
-    recordId="$1"
+updateDomainRecord() (
+    recordId=$(urlEncode "$1") || return 1
     rr=$(urlEncode "$2") || return 1
-    type="$3"
-    value="$4"
+    type=$(urlEncode "$3") || return 1
+    value=$(urlEncode "$4") || return 1
     canonicalQueryString="RR=${rr}&RecordId=${recordId}&Type=${type}&Value=${value}"
     rawJson=$(callAliDnsOpenApi "$canonicalQueryString" "UpdateDomainRecord" "$ACCESS_KEY_ID" "$ACCESS_KEY_SECRET") || return 2
     checkIfApiCallSuccess "$rawJson" || return 3
-}
+)
 
 # 读取配置文件，若配置文件不存在，则新建一个并退出
 readConfigFile() {
@@ -228,11 +234,26 @@ readConfigFile() {
 
         cat >"$configPath" <<EOL
 #!/bin/sh
+
+##################### 必填项 ###########################
+
+# 你的阿里云 Access Key ID
 ACCESS_KEY_ID=YourAccessKeyID
+# 你的阿里云 Access Key Secret
 ACCESS_KEY_SECRET=YourAccessKeySecret
+# 所有需要被关注的完整域名，用任意数量的英文逗号和空格分隔
 SUB_DOMAINS="www.example.com, example.com, www.例子.中国, 例子.中国"
+# 在域名已有 A 类解析记录的情况下，是否要将该记录的值更新为本机 IP
 IPV4_DDNS=true
+# 在域名已有 AAAA 类解析记录的情况下，是否要将该记录的值更新为本机 IP
 IPV6_DDNS=true
+
+##################### 选填项 ###########################
+
+# 自定义的获取本机 IPv4 的命令，若留空则为默认方式。一个例子是 "curl 4.ipw.cn"
+GET_IPV4_COMMAND=
+# 自定义的获取本机 IPv6 的命令，若留空则为默认方式
+GET_IPV6_COMMAND=
 EOL
 
         retVal=$?
@@ -252,8 +273,82 @@ EOL
     log info "已载入配置文件，准备正式执行脚本"
 }
 
+# 判断当前值是否需要更新，若需要则调 Open API
+# Param1: Record ID
+# Param2: RR
+# Param3: Type
+# Param4: Value
+# Param5: Ground truth value
+checkAndUpdateRecord() (
+    recordId="$1"
+    rr="$2"
+    type="$3"
+    value="$4"
+    gtValue="$5"
+
+    if [ "$value" = "$gtValue" ]; then
+        log info "\\033[1m${type} 类型\\033[22m: 解析记录与实际一致，不需要更新。记录值为「${gtValue}」"
+        return
+    fi
+
+    log info "\\033[1m${type} 类型\\033[22m: 即将更新记录。解析记录值为「${value}」，真实值应为「${gtValue}」"
+    updateDomainRecord "$recordId" "$rr" "$type" "$gtValue"
+)
+
+# 各个域名之间是独立处理、互不影响的，该函数处理单个域名
+handleSubDomain() (
+    subDomain="$1"
+    records=$(describeSubDomainRecords "$subDomain")
+
+    # 处理该子域名的每个记录
+    while read -r record; do
+        recordId=$(echon "$record" | cut -f 1)
+        rr=$(echon "$record" | cut -f 2)
+        type=$(echon "$record" | cut -f 3)
+        value=$(echon "$record" | cut -f 4)
+        gtValue="gtValue"
+
+        if [ "$type" = "A" ] && [ "$IPV4_DDNS" = "true" ]; then
+            if [ ${#GET_IPV4_COMMAND} -eq 0 ]; then
+                gtValue=$(getMachineIpv4)
+            else
+                gtValue=$(sh -c "$GET_IPV4_COMMAND")
+            fi
+        elif [ "$type" = "AAAA" ] && [ "$IPV6_DDNS" = "true" ]; then
+            if [ ${#GET_IPV6_COMMAND} -eq 0 ]; then
+                gtValue=$(getMachineIpv6)
+            else
+                gtValue=$(sh -c "$GET_IPV6_COMMAND")
+            fi
+        fi
+
+        if [ "$gtValue" = "gtValue" ]; then
+            log info "\\033[1m${type} 类型\\033[22m: 跳过该类型的记录"
+            continue
+        fi
+        if [ "$gtValue" = "" ]; then
+            log warn "\\033[1m${type} 类型\\033[22m: 获取到的真实 IP 地址为空，跳过此条记录"
+            continue
+        fi
+
+        checkAndUpdateRecord "$recordId" "$rr" "$type" "$value" "$gtValue"
+    done <<EOL
+${records}
+EOL
+)
+
 main() {
     readConfigFile
+
+    SUB_DOMAINS=$(echon "$SUB_DOMAINS" | tr ',' ' ')
+
+    subDomainCount=0
+    for subDomain in $SUB_DOMAINS; do
+        subDomainCount=$((subDomainCount + 1))
+        log info "-------------------------------------------------------------------------------"
+        log info "开始处理域名#${subDomainCount}: \\033[4m${subDomain}\\033[24m"
+        handleSubDomain "$subDomain"
+    done
 }
 
 main
